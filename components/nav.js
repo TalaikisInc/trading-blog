d290c3b21f5150d72afe701ab3f2129ffa85281d
Tadas Talaikis <talaikis.tadas@gmail.com>
2020-03-20 14:11:27 +0200

    add pages

--- components/nav.js
+++ components/nav.js
@@ -3,9 +3,21 @@ import Link from 'next/link'
 
 const Nav = () => (
   <nav>
-    <Link href="/about">
-      <a>About</a>
-    </Link>
+    <div className="nav-link">
+      <Link href="/performance">
+        <a>Performance</a>
+      </Link>
+    </div>
+    <div className="nav-link">
+      <Link href="/faq">
+        <a>FAQ</a>
+      </Link>
+    </div>
+    <div className="nav-link">
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
   </nav>
 )
 
