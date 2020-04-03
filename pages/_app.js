99314f35ec3ecefd6eaffbadae4a892e99fee72c
Tadas Talaikis <talaikis.tadas@gmail.com>
2020-04-03 12:39:00 +0300

    change date & add post

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import App from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Router from 'next/router'
 
+import { pageView } from '../posts/gtag'
 import './style.css'
+Router.events.on('routeChangeComplete', (url) => pageView(url))
 
 class Custom extends App {
   render () {
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '../posts/gtag'
+
 class MyDocument extends Document {
   static async getInitialProps (ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -10,7 +12,19 @@ class MyDocument extends Document {
   render () {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+          }} />
+        </Head>
         <body>
           <Main />
           <NextScript />
